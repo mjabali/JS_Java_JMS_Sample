0c2da81305d04e8bc1a555d0887d7fc315cea6b3
Marcelo Jabali <marcelojabali@yahoo.com>
2012-02-29 11:19:03 -0800

    Created Java client

--- src/com/kaazing/sample/java/client/MyGatewayJavaClient.java
+++ src/com/kaazing/sample/java/client/MyGatewayJavaClient.java
@@ -3,6 +3,15 @@
  */
 package com.kaazing.sample.java.client;
 
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.*;
+
+
 /**
  * @author Marcelo
  *
@@ -12,9 +21,34 @@ public class MyGatewayJavaClient {
 	/**
 	 * @param args
 	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Properties props = new Properties();
+		props.put(InitialContext.INITIAL_CONTEXT_FACTORY, "com.kaazing.gateway.jms.client.stomp.StompInitialContextFactory");
+		props.put(Context.PROVIDER_URL, "ws://localhost:8001/jms");
+		try {
+			InitialContext ctx = new InitialContext(props);
+			ConnectionFactory connectionFactory = (ConnectionFactory)ctx.lookup("ConnectionFactory");
+			Connection connection = connectionFactory.createConnection(null, null);
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Topic topic = (Topic)ctx.lookup("/topic/myTopic");
+			MessageConsumer consumer = session.createConsumer(topic);
+			consumer.setMessageListener(new MessageListener(){
+				@Override
+				public void onMessage(Message msg){
+					try{
+						System.out.println("MESSAGE: " + ((TextMessage)msg).getText());
+					}catch(JMSException jmse){
+						jmse.printStackTrace();
+					}
+				}
+			});
+			System.out.println("Starting WebSocket Connection...");
+			connection.start();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
